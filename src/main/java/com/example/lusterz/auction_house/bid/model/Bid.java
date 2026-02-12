534b00e44c7f1bd664a7ed84b8b9f1febc189d2c
the thing <65288778+mohamedfffff@users.noreply.github.com>
2026-02-12 17:16:50 +0200

    finished bid implementation (for-now)

--- src/main/java/com/example/lusterz/auction_house/bid/model/Bid.java
+++ src/main/java/com/example/lusterz/auction_house/bid/model/Bid.java
@@ -14,9 +14,15 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
     public class Bid {
     
