4fa8c14156abdedbaf74c6a1417494c8b2636a40
the thing <65288778+mohamedfffff@users.noreply.github.com>
2026-03-11 13:08:45 +0200

    removed services interfaces and only use services classes with no override

--- src/main/java/com/example/lusterz/auction_house/modules/bid/service/BidService.java
+++ src/main/java/com/example/lusterz/auction_house/modules/bid/service/BidService.java
@@ -1,21 +1,144 @@
 package com.example.lusterz.auction_house.modules.bid.service;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.lusterz.auction_house.common.exception.ItemException;
+import com.example.lusterz.auction_house.common.exception.BidException;
+import com.example.lusterz.auction_house.common.exception.UserException;
 import com.example.lusterz.auction_house.modules.bid.dto.BidDto;
 import com.example.lusterz.auction_house.modules.bid.dto.BidRequest;
+import com.example.lusterz.auction_house.modules.bid.mapper.BidMapper;
+import com.example.lusterz.auction_house.modules.bid.model.Bid;
+import com.example.lusterz.auction_house.modules.bid.repository.BidRepository;
+import com.example.lusterz.auction_house.modules.item.model.AuctionStatus;
+import com.example.lusterz.auction_house.modules.item.model.Item;
+import com.example.lusterz.auction_house.modules.item.repository.ItemRepository;
+import com.example.lusterz.auction_house.modules.user.model.User;
+import com.example.lusterz.auction_house.modules.user.repository.UserRepository;
 
-public interface BidService {
 
-    BidDto getBid(Long id);
+@Slf4j
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class BidService {
 
-    List<BidDto> getAllBids();
+    private final UserRepository userRepository;
+    private final ItemRepository itemRepository;
+    private final BidRepository bidRepository;
+    private final BidMapper bidMapper;
 
-    List<BidDto> getAllBidsByBidderId(Long bidderId);
+    public BidDto getBid(Long id) {
+        Bid bid = bidRepository.findById(id)
+            .orElseThrow(() -> BidException.NotFound.byId(id));
+        return bidMapper.toDto(bid);
+    }
 
-    List<BidDto> getAllBidsByItemId(Long itemId);
+    public List<BidDto> getAllBids() {
+        return bidRepository.findAll()
+                .stream()
+                .map(bidMapper::toDto)
+                .toList();
+    }
 
-    BidDto placeBid(BidRequest bidRequest);
+    public List<BidDto> getAllBidsByBidderId(Long bidderId) {
+        if (!userRepository.existsById(bidderId)) {
+            throw UserException.NotFound.byId(bidderId);
+        }
+        return bidRepository.findAllByBidderId(bidderId)
+                .stream()
+                .map(bidMapper::toDto)
+                .toList();
+    }
 
-    void deleteBid(Long bidderId, Long itemId, Long bidId);
+    public List<BidDto> getAllBidsByItemId(Long itemId) {
+        if (!itemRepository.existsById(itemId)) {
+            throw ItemException.NotFound.byId(itemId);
+        }
+        return bidRepository.findAllByItemId(itemId)
+                .stream()
+                .map(bidMapper::toDto)
+                .toList();
+    }
+
+    @Transactional
+    public BidDto placeBid(BidRequest bidRequest) {
+        User bidder = userRepository.findById(bidRequest.bidderId())
+            .orElseThrow(() -> UserException.NotFound.byId(bidRequest.bidderId()));
+        Item item = itemRepository.findById(bidRequest.itemId())
+            .orElseThrow(() -> ItemException.NotFound.byId(bidRequest.itemId()));
+
+        if (item.getSeller().getId().equals(bidRequest.bidderId())) {
+            throw BidException.Unauthorized.isOwner();
+        }
+
+        if (bidder.getBalance().compareTo(bidRequest.amount()) < 0) {
+            throw UserException.InsufficientFunds.balanceLessThanBid(bidRequest.amount());
+        }
+
+        if (!item.getStatus().equals(AuctionStatus.ACTIVE)) {
+            throw ItemException.InvalidState.notActive();
+        }
+
+        if (item.getCurrentHighestBid().compareTo(bidRequest.amount()) > 0 ) {
+            throw BidException.InsufficientBid.lessThanHighest();
+        }
+
+
+        Bid newBid = Bid.builder()
+            .amount(bidRequest.amount())
+            .bidder(bidder)
+            .item(item)
+            .bidTime(OffsetDateTime.now())
+            .build();
+
+        // update the highest bid on item entity
+        item.setCurrentHighestBid(bidRequest.amount());
+
+        log.info("Bid {} placed on item {} from user {}", newBid.getAmount(), item.getTitle(), bidder.getUsername());
+
+        bidRepository.save(newBid);
+        return bidMapper.toDto(newBid);
+    }
+
+    @Transactional
+    public void deleteBid(Long bidderId, Long itemId, Long bidId) {
+        Item item = itemRepository.findById(itemId)
+            .orElseThrow(() -> ItemException.NotFound.byId(itemId));
+        Bid deletedBid = bidRepository.findById(bidId)
+            .orElseThrow(() -> BidException.NotFound.byId(bidId));
+
+        if (!deletedBid.getBidder().getId().equals(bidderId)) {
+            throw BidException.Unauthorized.notOwner();
+        }
+
+        if (!item.getStatus().equals(AuctionStatus.ACTIVE)) {
+            throw ItemException.InvalidState.notActive();
+        }
+
+
+        bidRepository.delete(deletedBid);
+
+        // if the deleted bid is the highest then we need to update the item
+        if (deletedBid.getAmount().equals(item.getCurrentHighestBid())) {
+            updateHighestBid(item);
+        }
+        
+    }
+    
+    private void updateHighestBid(Item item) {
+        BigDecimal highest = bidRepository.findTopByItemOrderByAmountDesc(item)
+            .map(Bid::getAmount)
+            .orElse(item.getStartingPrice());
+
+        item.setCurrentHighestBid(highest);
+    }
+
 }
--- src/main/java/com/example/lusterz/auction_house/modules/user/service/UserService.java
+++ src/main/java/com/example/lusterz/auction_house/modules/user/service/UserService.java
@@ -3,38 +3,156 @@ package com.example.lusterz.auction_house.modules.user.service;
 import java.math.BigDecimal;
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.lusterz.auction_house.common.exception.AuthException;
+import com.example.lusterz.auction_house.common.exception.UserException;
+import com.example.lusterz.auction_house.modules.auth.model.AuthProviders;
 import com.example.lusterz.auction_house.modules.user.dto.UserPrivateDto;
 import com.example.lusterz.auction_house.modules.user.dto.UserPublicDto;
 import com.example.lusterz.auction_house.modules.user.dto.UserUpdatePasswordRequest;
 import com.example.lusterz.auction_house.modules.user.dto.UserUpdateRequest;
 import com.example.lusterz.auction_house.modules.user.dto.UserUpdateRoleRequest;
+import com.example.lusterz.auction_house.modules.user.mapper.UserMapper;
 import com.example.lusterz.auction_house.modules.user.model.User;
+import com.example.lusterz.auction_house.modules.user.model.UserCredential;
+import com.example.lusterz.auction_house.modules.user.repository.UserRepository;
 
-public interface UserService {
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
  
-    UserPrivateDto getUserById(Long id);
+    public UserPrivateDto getUserById(Long id) {
+        User user = userRepository.findById(id)
+            .orElseThrow(() -> UserException.NotFound.byId(id));
+        return userMapper.toPrivateDto(user);
+    }
 
-    UserPublicDto getUserByName(String username);
+    public UserPublicDto getUserByName(String username) {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> UserException.NotFound.byUsername(username));
+        return userMapper.toPublicDto(user); 
+    }
 
-    User getByUsernameOrEmail(String identifier);
+    public User getByUsernameOrEmail(String identifier) {
+        User user = userRepository.findByUsernameOrEmail(identifier, identifier)
+            .orElseThrow(() -> UserException.NotFound.byIdentifier(identifier));
+        return user;
+    }
+    
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
 
-    boolean existsByEmail(String email);
+    public List<UserPrivateDto> getAllActiveUsers() {
+        List<User> list = userRepository.findAllByActive(true);
+        return list.stream()
+                .map(userMapper::toPrivateDto)
+                .toList();
+    }
 
-    List<UserPrivateDto> getAllActiveUsers();
+    public List<UserPrivateDto> getAllUnactiveUsers() {
+        List<User> list = userRepository.findAllByActive(false);
+        return list.stream()
+                .map(userMapper::toPrivateDto)
+                .toList();
+    }
 
-    List<UserPrivateDto> getAllUnactiveUsers();
+    public List<UserPrivateDto> getAllUsers() {
+        List<User> list = userRepository.findAll();
+        return list.stream()
+                .map(userMapper::toPrivateDto)
+                .toList();
+    }
 
-    List<UserPrivateDto> getAllUsers();
 
-    UserPrivateDto updateUser(Long id, UserUpdateRequest request);
+    @Transactional
+    public UserPrivateDto updateUser(Long id, UserUpdateRequest userRequest) {
+        User existingUser = userRepository.findById(id)
+            .orElseThrow(() -> UserException.NotFound.byId(id));
 
-    void deactivateUser(Long id);
+        if (!existingUser.getUsername().equals(userRequest.username()) &&
+             userRepository.existsByUsername(userRequest.username())) {
+            throw UserException.AlreadyExists.byUsername(userRequest.username());
+        }
+        if (!existingUser.getEmail().equals(userRequest.email()) &&
+             userRepository.existsByEmail(userRequest.email())) {
+            throw UserException.AlreadyExists.byEmail(userRequest.email());
+        }
 
-    void updatePassword(Long id, UserUpdatePasswordRequest request);
+        existingUser.setUsername(userRequest.username());
+        existingUser.setEmail(userRequest.email());
+        existingUser.setUserImageUrl(userRequest.userImageUrl());
 
-    void updateRole(Long id, UserUpdateRoleRequest newRole);
+        userRepository.save(existingUser);
 
-    //to-do
-    void updateBalance(Long id, BigDecimal amount);
+        return userMapper.toPrivateDto(existingUser);
+    }
 
-}   
+    @Transactional
+    public void deactivateUser(Long id) {
+        User deletedUser = userRepository.findById(id)
+            .orElseThrow(() -> UserException.NotFound.byId(id));
+
+        //to-do check if request user id matches the id
+        // Long currentUserId = ((CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+
+        // if (!deletedUser.getId().equals(currentUserId)) {
+        //     throw UserException.Unauthorized.notOwner();
+        // }
+
+        deletedUser.setActive(false);
+        userRepository.save(deletedUser);
+    }
+
+    @Transactional
+    public void updatePassword(Long id, UserUpdatePasswordRequest request) {
+        User user = userRepository.findById(id)
+            .orElseThrow(() -> UserException.NotFound.byId(id));
+
+        UserCredential localCredential = user.getUserCredentials()
+            .stream()
+            .filter(l -> AuthProviders.LOCAL.equals(l.getProvider()))
+            .findFirst()
+            .orElseThrow(() -> AuthException.Provider.notLocal());
+
+        if (!passwordEncoder.matches(request.oldPassword(), localCredential.getProviderId())) {
+            throw UserException.PasswordMismatch.oldAndGiven();
+        }
+
+        if (!request.newPassword().equals(request.confirmPassword())) {
+            throw UserException.PasswordMismatch.newAndConfirm();
+        }
+
+        localCredential.setProviderId(passwordEncoder.encode(request.newPassword()));
+    }
+
+    @Transactional
+    public void updateRole(Long id, UserUpdateRoleRequest request) {
+        User user = userRepository.findById(id)
+            .orElseThrow(() -> UserException.NotFound.byId(id));
+
+        user.setRole(request.role());
+        userRepository.save(user);
+
+        refreshSecurityContext(user);
+    }
+
+    @Transactional
+    public void updateBalance(Long id, BigDecimal amount) {
+        //to-do 
+    }
+
+    private void refreshSecurityContext(User user) {
+        //to-do
+    }
+
+}
