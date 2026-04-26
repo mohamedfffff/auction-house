53861b618b7cdc17f02742871d7df7db5914c797
the thing <65288778+mohamedfffff@users.noreply.github.com>
2026-04-26 08:55:04 +0300

    finished query unit test cases for itemService

--- src/test/java/com/example/lusterz/auction_house/unit/item/ItemServiceTest.java
+++ src/test/java/com/example/lusterz/auction_house/unit/item/ItemServiceTest.java
@@ -23,6 +23,7 @@ import org.springframework.context.ApplicationEventPublisher;
 
 import com.example.lusterz.auction_house.TestData;
 import com.example.lusterz.auction_house.common.exception.ItemException;
+import com.example.lusterz.auction_house.common.exception.UserException;
 import com.example.lusterz.auction_house.modules.bid.repository.BidRepository;
 import com.example.lusterz.auction_house.modules.item.dto.ItemDto;
 import com.example.lusterz.auction_house.modules.item.mapper.ItemMapper;
@@ -101,6 +102,86 @@ public class ItemServiceTest {
         assertTrue(result.isEmpty());
 
         verify(itemRepository).findAll();
+        verifyNoInteractions(itemMapper);
+    }
+
+    @Test
+    void getActiveAuctions_ReturnDtoList_WhenFound() {
+        AuctionStatus status = AuctionStatus.ACTIVE;
+        Item item = TestData.testItem(1L, status);
+        ItemDto dto = TestData.testItemDto(item);
+
+        when(itemRepository.findAllByStatus(status)).thenReturn(List.of(item));
+
+        List<ItemDto> result = itemService.getActiveAuctions();
+
+        assertEquals(1, result.size());
+        assertEquals(dto, result.get(0));
+
+        verify(itemRepository).findAllByStatus(status);
+        verify(itemMapper, times(1)).toDto(any(Item.class));
+    }
+
+    @Test
+    void getActiveAuctions_ReturnEmptyList_WhenNotFound() {
+        AuctionStatus status = AuctionStatus.ACTIVE;
+
+        when(itemRepository.findAllByStatus(status)).thenReturn(List.of());
+
+        List<ItemDto> result = itemService.getActiveAuctions();
+
+        assertTrue(result.isEmpty());
+
+        verify(itemRepository).findAllByStatus(status);
+        verifyNoInteractions(itemMapper);
+    }
+
+    @Test
+    void getAllItemsBySellerId_ReturnDtoList_WhenFoundAndIdExists() {
+        Long sellerId = 1L;
+        Item item = TestData.testItem(1L, AuctionStatus.ACTIVE);
+        ItemDto dto = TestData.testItemDto(item);
+
+        when(userRepository.existsById(sellerId)).thenReturn(true);
+        when(itemRepository.findAllBySellerId(sellerId)).thenReturn(List.of(item));
+
+        List<ItemDto> result = itemService.getAllItemsBySellerId(sellerId);
+
+        assertEquals(1, result.size());
+        assertEquals(dto, result.get(0));
+
+        verify(userRepository).existsById(sellerId);
+        verify(itemRepository).findAllBySellerId(sellerId);
+        verify(itemMapper, times(1)).toDto(item);
+    }
+
+    @Test
+    void getAllItemsBySellerId_ReturnEmptyList_WhenNotFoundAndIdExists() {
+        Long sellerId = 1L;
+
+        when(userRepository.existsById(sellerId)).thenReturn(true);
+        when(itemRepository.findAllBySellerId(sellerId)).thenReturn(List.of());
+
+        List<ItemDto> result = itemService.getAllItemsBySellerId(sellerId);
+
+        assertTrue(result.isEmpty());
+
+        verify(userRepository).existsById(sellerId);
+        verify(itemRepository).findAllBySellerId(sellerId);
+        verifyNoInteractions(itemMapper);
+    }
+
+    @Test
+    void getAllItemsBySellerId_ThrowUserExceptionNotFound_WhenIdNotFound() {
+        Long id = 1L;
+
+        when(userRepository.existsById(id)).thenReturn(false);
+
+        UserException.NotFound ex = assertThrows(UserException.NotFound.class, () -> itemService.getAllItemsBySellerId(id));
+        assertTrue(ex.getMessage().contains(id.toString()));
+
+        verify(userRepository).existsById(id);
+        verifyNoInteractions(itemRepository);
         verifyNoInteractions(itemMapper);
     }
 }
